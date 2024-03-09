0b11e24d22c75022711358ed288b3cf593b6b5b1
dartungar <dartungar@gmail.com>
2024-03-09 12:51:53 +0400

    feat: implement deleting entries

--- src/filesIntegration/createFileModal.ts
+++ src/filesIntegration/createFileModal.ts
@@ -4,7 +4,7 @@ import { ConfirmationModal } from "src/common/confirmationModal";
 export class CreateFileModal extends ConfirmationModal {
 	constructor(app: App, path: string, content: string) {
 		super(app, "File does not exist", () => this.createFile(path, content));
-        this.setContent(`The file at location "${path}" doesn't exist.\nWould you like to create it?`)
+    this.setContent(`The file at location "${path}" doesn't exist.\nWould you like to create it?`)
 	}
 
     async createFile(path: string, content: string): Promise<void> {
